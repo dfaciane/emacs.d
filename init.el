cd72e588aae75da3ce6068c58255c50c0167eac6
Dave Faciane <dave.faciane@weather.com>
2018-02-10 16:46:03 -0500

    Initial version.

---
+++ init.el
@@ -0,0 +1,136 @@
+;;
+;;
+
+;; Hide menus, toolbars, scrollbars, splashscreens
+;;(menu-bar-mode -1)
+;; (tool-bar-mode -1)
+;;(scroll-bar-mode -1)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+
+;; line number mode
+;; M-x linum-mode.
+;; To enable it globally for all buffers run M-x global-linum-mode.
+;; To enable it globally and permanently add this to your .emacs file:
+;; (global-linum-mode 1)
+;;
+;; Now you can customize it further with:
+;; M-x customize-group RET linum RET
+
+;; IDO Mode
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+;; IF you want Ido mode to work with C-x C-f (find-files) then add this as well:
+(setq ido-everywhere t)
+
+;; auto-indent
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;;customize the Font and Background
+;; To customize the default font and color, type M-x customize-face RET default RET.
+;; To customize the default syntax highlighter (also called “font locking”) typeM-x customize-group RET font-lock-faces RET.
+
+;; You can change the path here
+;;(add-to-list 'load-path "~/.emacs.d/")
+;; (load-library "packagefilename")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("marmalade" . "http://marmalade-repo.org/packages/")
+     ("melpa" . "http://melpa.org/packages/"))))
+ '(package-selected-packages (quote (better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; Temporarily show line numbers when jumping to line
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
+;; full screen magit-status
+(require 'magit)
+
+;; Magit stuff
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+;; magnars
+(defun cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a before-save-hook, and that
+might be bad."
+  (interactive)
+  (untabify (point-min) (point-max))
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+;; Various superfluous white-space. Just say no.
+(add-hook 'before-save-hook 'cleanup-buffer-safe)
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (cleanup-buffer-safe)
+  (indent-region (point-min) (point-max)))
+
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
