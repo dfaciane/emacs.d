9f8524a862203b29d631ac6e85e925781c204145
Dave Faciane <dave.faciane@weather.com>
2018-02-14 13:49:30 +0000

    Magit keybinding.

--- init.el
+++ init.el
@@ -77,6 +77,8 @@
 ;; full screen magit-status
 
 ;; Magit stuff
+(global-set-key (kbd "C-x g s") 'magit-status)
+
 (defun magit-quit-session ()
   "Restores the previous window configuration and kills the magit buffer"
   (interactive)
@@ -137,3 +139,17 @@ Including indent-buffer, which should not be called automatically on save."
   (indent-region (point-min) (point-max)))
 
 (global-set-key (kbd "C-c n") 'cleanup-buffer)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
