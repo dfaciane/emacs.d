4290ac5f8b958b25069b8ef54fc8d50b5ef869ba
Dave Faciane <dave.faciane@weather.com>
2018-02-15 01:08:24 -0500

    Add /custom dir.

--- init.el
+++ init.el
@@ -139,20 +139,3 @@ Including indent-buffer, which should not be called automatically on save."
   (indent-region (point-min) (point-max)))
 
 (global-set-key (kbd "C-c n") 'cleanup-buffer)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" default)))
- '(package-selected-packages
-   (quote
-    (hc-zenburn-theme zenburn-theme use-package solarized-theme magit helm diminish better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
