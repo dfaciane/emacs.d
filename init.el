b18dbaed2c10d45825b9fa71b6a90e078c65d9f3
Dave Faciane <dave.faciane@weather.com>
2018-02-16 00:54:12 +0000

    Add some window management stuff.

--- init.el
+++ init.el
@@ -176,6 +176,10 @@ Including indent-buffer, which should not be called automatically on save."
 ;; repeat last macro
 (global-set-key [f5] 'call-last-kbd-macro)
 
+;; window management
+(windmove-default-keybindings)
+(winner-mode 1)
+
 
 ;; Theme du jour
 
