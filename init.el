3850377a332aee53c4f6fefff10669fd27b7a748
Dave Faciane <dave.faciane@weather.com>
2018-02-24 10:30:21 -0500

    Add Projectile.

--- init.el
+++ init.el
@@ -102,6 +102,9 @@
 (with-eval-after-load 'flycheck
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
+(use-package projectile)
+(projectile-mode)
+
 ;; better window management with winner and windmove
 
 (windmove-default-keybindings)
@@ -322,10 +325,11 @@ Including indent-buffer, which should not be called automatically on save."
 (global-set-key (kbd "C-c n") 'cleanup-buffer)
 
 ;; running emacs in daemon mode so don't need these
-;;(global-unset-key (kbd "C-x C-c"))
-;;(global-unset-key (kbd "C-x C-z"))
+(global-unset-key (kbd "C-x C-c"))
+(global-unset-key (kbd "C-x C-z"))
 
-;;  Steve Yegge from "effective emacs"
+
+;;  Steve Yegge "effective emacs"
 ;;
 ;;
 (use-package region-bindings-mode)
@@ -339,7 +343,7 @@ Including indent-buffer, which should not be called automatically on save."
 
 ;; Expand region (increases selected region by semantic units)
 (use-package expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
+;(global-set-key (kbd "M-=") 'er/expand-region)
 (key-chord-define-global ";e" 'er/expand-region)
 
 ;;
