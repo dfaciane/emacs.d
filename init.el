e0900ec4a2d937458ccac4d7fd9d6d4f87ea7f40
Dave Faciane <dave.faciane@weather.com>
2018-05-14 14:21:58 -0400

    Add multiple-cursors.

--- init.el
+++ init.el
@@ -178,6 +178,10 @@
 (global-set-key (kbd "C-,") 'completion-at-point)
 (bind-key "M-/" 'hippie-expand)
 
+;; multiple cursors
+(use-package multiple-cursors)
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
+
 ;; join the current line and the next one
 (global-set-key (kbd "M-j")
             (lambda ()
@@ -499,14 +503,69 @@ Including indent-buffer, which should not be called automatically on save."
 
 ;; tramp mode
 (require 'tramp)
-(add-to-list 'tramp-remote-path "/home/dfaciane/bin")
-(add-to-list 'tramp-remote-path 'tramp-default-remote-path)
+;;(add-to-list 'tramp-remote-path "/home/dfaciane/bin")
+;;(add-to-list 'tramp-remote-path 'tramp-default-remote-path)
 (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
 ;;(push "/home/dfaciane/bin/" tramp-remote-path)
-
-
-
 ;; (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+
+;; eww web browser
+(setq browse-url-browser-function 'eww-browse-url)
+(global-set-key (kbd "C-c b") 'browse-url-at-point)
+
+(use-package eww
+  :defer t
+  :init
+  (setq browse-url-browser-function
+        '((".*google.*maps.*" . browse-url-generic)
+          ;; Github goes to firefox, but not gist
+          ("http.*\/\/github.com" . browse-url-generic)
+          ("groups.google.com" . browse-url-generic)
+          ("docs.google.com" . browse-url-generic)
+          ("melpa.org" . browse-url-generic)
+          ("stackoverflow\.com" . browse-url-generic)
+          ("t.co" . browse-url-generic)
+          ("twitter.com" . browse-url-generic)
+          ("youtube.com" . browse-url-generic)
+          ("amazon.com" . browse-url-generic)
+          ("slideshare.net" . browse-url-generic)
+          ("." . eww-browse-url)))
+  (setq shr-external-browser 'browse-url-generic)
+  (setq browse-url-generic-program (executable-find "chrome"))
+  (add-hook 'eww-mode-hook #'toggle-word-wrap)
+  (add-hook 'eww-mode-hook #'visual-line-mode)
+  :config
+  (use-package s :ensure t)
+  (define-key eww-mode-map "o" 'eww)
+  (define-key eww-mode-map "O" 'eww-browse-with-external-browser)
+  (define-key eww-mode-map "j" 'next-line)
+  (define-key eww-mode-map "k" 'previous-line)
+
+  (use-package eww-lnum
+    :ensure t
+    :config
+    (bind-key "f" #'eww-lnum-follow eww-mode-map)
+    (bind-key "U" #'eww-lnum-universal eww-mode-map)))
+
+;;(use-package link-hint
+;;  :ensure t
+;;  :bind ("C-c f" . link-hint-open-link))
+
+;; search backwards, prompting to open any URL found.
+(defun browse-last-url-in-brower ()
+  (interactive)
+  (save-excursion
+    (ffap-next-url t t)))
+
+(global-set-key (kbd "C-c u") 'browse-last-url-in-brower)
+
+;; render the current buffer as html and display
+(defun eww-render-current-buffer ()
+  "Render HTML in the current buffer with EWW"
+  (interactive)
+  (beginning-of-buffer)
+  (eww-display-html 'utf8 (buffer-name)))
+
 
 ;; Emacs server
 ;;(use-package server)
